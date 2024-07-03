84cf7051d10968e96e7287fda4263983feffc6e2
jesus <jesus_estrada5@my.vcccd.edu>
2024-07-02 21:45:08 -0700

    CS V11

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def findEvenNumber(numbers):
         if num % 2 == 0:
             yield num
 
-
 def main():
     numbers = [random.randint(0, 100) for i in range(10)]
     print('Original list', numbers)
